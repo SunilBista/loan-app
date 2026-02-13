9bc22cffaf11bc2d3cb2cef7c22611c26eeca5de
SunilBista <sunilbista303@gmail.com>
2026-02-13 21:09:56 +1100

    chore: add utils for currency and date

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(amount: number, currency: string = "AUD") {
+  return new Intl.NumberFormat("en-AU", {
+    style: "currency",
+    currency,
+  }).format(amount);
+}
+
+export function formatDate(dateString: string) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  return new Date(dateString).toLocaleDateString("en-AU", options);
 }
