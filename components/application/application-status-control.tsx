b394020c1458835220b32b9137189e076a645f73
SunilBista <sunilbista303@gmail.com>
2026-02-14 15:53:30 +1100

    feat: update logic to change status and add dialog confirmation

--- components/application/application-status-control.tsx
+++ components/application/application-status-control.tsx
@@ -10,6 +10,14 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "@/components/ui/dialog";
 import { ApplicationStatus } from "@/lib/type";
 import { StatusBadge } from "../ui/status-badge";
 import { Card, CardHeader, CardTitle } from "../ui/card";
@@ -17,19 +25,31 @@ import { useLoanApplication } from "@/context/loan-application-context";
 import { DEFAULT_STATUS_NOTES } from "@/lib/mock-db";
 import { isValidTransition } from "@/context/utils";
 import { toast } from "sonner";
+
 interface ApplicationStatusControlProps {
   initialStatus: ApplicationStatus;
 }
+
 const ApplicationStatusControl = ({
   initialStatus,
 }: ApplicationStatusControlProps) => {
   const [status, setStatus] = useState<ApplicationStatus>(initialStatus);
+  const [selectedStatus, setSelectedStatus] =
+    useState<ApplicationStatus>(initialStatus);
+  const [open, setOpen] = useState(false);
 
   const isLocked = status === "Approved" || status === "Rejected";
-  const { updateStatus, selectedId, resetApplication, selectedApplication } =
-    useLoanApplication();
 
-  const handleStatusChange = (newStatus: ApplicationStatus) => {
+  const {
+    updateStatus,
+    selectedId,
+    resetApplication,
+    selectedApplication,
+    getPreviousStatus,
+  } = useLoanApplication();
+
+  const handleStatusChange = () => {
+    setOpen(false);
     if (!selectedApplication?.currentStatus) {
       toast.error("Invalid transition", {
         description:
@@ -38,45 +58,43 @@ const ApplicationStatusControl = ({
       return;
     }
 
-    if (!isValidTransition(selectedApplication.currentStatus, newStatus)) {
+    if (!isValidTransition(selectedApplication.currentStatus, selectedStatus)) {
+      setSelectedStatus(selectedApplication.currentStatus);
       toast.error("Invalid transition", {
-        description: `Cannot move from "${selectedApplication?.currentStatus}" to "${newStatus}".`,
+        description: `Cannot move from "${selectedApplication.currentStatus}" to "${selectedStatus}".`,
       });
       return;
     }
-    if (selectedApplication.currentStatus === newStatus) {
+
+    if (selectedApplication.currentStatus === selectedStatus) {
       toast.warning("No change", {
-        description: `The application is already "${newStatus}".`,
+        description: `The application is already "${selectedStatus}".`,
       });
       return;
     }
 
     if (selectedId) {
-      updateStatus(selectedId, newStatus, DEFAULT_STATUS_NOTES[newStatus]);
-      setStatus(newStatus);
-      if (newStatus === "Approved") {
-        toast.success(`Status updated to "${newStatus}"`, {
-          description: "Application has been approved.",
-        });
-      } else if (newStatus === "Under Review") {
-        toast.info(`Status updated to "${newStatus}"`, {
-          description: "Application is now under review.",
-        });
-      } else if (newStatus === "Rejected") {
-        toast.info(`Status updated to "${newStatus}"`, {
-          description: "Application has been rejected.",
-        });
-      }
+      updateStatus(
+        selectedId,
+        selectedStatus,
+        DEFAULT_STATUS_NOTES[selectedStatus],
+      );
+
+      setStatus(selectedStatus);
+      setOpen(false);
+
+      toast.success(`Status updated to "${selectedStatus}"`);
     }
   };
 
   const resetStatus = () => {
-    setStatus(initialStatus);
     if (selectedId) {
       resetApplication(selectedId);
       toast.info("Application reset", {
         description: "The application has been restored to its initial state.",
       });
+      setStatus(getPreviousStatus(selectedId) || initialStatus);
+      setSelectedStatus(getPreviousStatus(selectedId) || initialStatus);
     }
   };
 
@@ -84,9 +102,6 @@ const ApplicationStatusControl = ({
     <div className="bg-white shadow rounded-lg p-4 flex flex-col justify-between">
       <div>
         <h2 className="font-semibold text-lg mb-4">Application Status</h2>
-        <p className="text-gray-500 mb-4">
-          Progress this application to the next stage.
-        </p>
 
         <p className="mb-4">
           Current:{" "}
@@ -95,11 +110,14 @@ const ApplicationStatusControl = ({
             className="inline-flex items-center gap-2"
           />
         </p>
+        {!isLocked && (
+          <p className="text-gray-500 mb-4">Review this application.</p>
+        )}
 
         <Select
-          value={status}
+          value={selectedStatus}
           onValueChange={(value) =>
-            handleStatusChange(value as ApplicationStatus)
+            setSelectedStatus(value as ApplicationStatus)
           }
           disabled={isLocked}
         >
@@ -123,11 +141,48 @@ const ApplicationStatusControl = ({
             </SelectGroup>
           </SelectContent>
         </Select>
+
+        {!isLocked && selectedStatus !== status && (
+          <Button
+            className="mt-4 w-full sm:w-auto cursor-pointer bg-green-600 hover:bg-green-700"
+            onClick={() => setOpen(true)}
+          >
+            Submit
+          </Button>
+        )}
       </div>
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Confirm Status Change</DialogTitle>
+            <DialogDescription>
+              Are you sure you want to change status to{" "}
+              <strong>{selectedStatus}</strong>?
+            </DialogDescription>
+          </DialogHeader>
+
+          <DialogFooter>
+            <Button
+              variant="outline"
+              className="cursor-pointer"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </Button>
+            <Button
+              className="bg-green-600 hover:bg-green-700 cursor-pointer"
+              onClick={handleStatusChange}
+            >
+              Confirm
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
 
       {isLocked && (
         <>
-          <Card>
+          <Card className="mt-2">
             <CardHeader>
               <CardTitle className="text-grey-500 text-sm">
                 No further status changes are allowed.
